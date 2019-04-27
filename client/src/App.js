8dfebf55d06952d9664145e2d683978c54829c7a
Mark Gardner <mark.gardner@endurance.com>
2019-04-27 10:45:54 -0500

    skeleton React pages and routes for Search and Saved

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Jumbotron from "./components/Jumbotron";
+import Nav from "./components/Nav";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
 
 class App extends Component {
   render() {
     return (
-      <div>
-      </div>
+      <BrowserRouter>
+        <Fragment>
+          <Nav />
+          <Jumbotron>
+            <h1>(React) Google Books Search</h1>
+          </Jumbotron>
+          <Switch>
+            <Route exact path="/" component={Search} />
+            <Route exact path="/search" component={Search} />
+            <Route exact path="/saved" component={Saved} />
+          </Switch>
+        </Fragment>
+      </BrowserRouter>
     );
   }
 }
